d550ad6134cf8f3c3141e08a9e6efb0810bcb0f9
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-05 20:22:08 -0400

    commit tree 54785abe479a404a515d009f6f0b7c9d02cd4da6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+// const superbowlWin = (array) => {
+//     let result = array.find((o) => o.result === "W")
+//     if(result !== undefined) {
+//         return result.year;
+//     }
+// }
+
+function superbowlWin(array) {
+    const chosenElement = array.find(({ result }) => {
+        return result === "W";
+    })
+    return chosenElement ? chosenElement.year : undefined
+}
